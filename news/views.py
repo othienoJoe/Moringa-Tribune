081388c8c2c8496c419e4e1a13765a2a1b82b262
othienoJoe <bryo094@gmail.com>
2021-10-12 18:18:18 +0300

    created template folder with a file and updated the settings file

--- news/views.py
+++ news/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
 
-def news_of_day(request):
-    date = dt.date.today()
+def news_of_day(request,past_date):
+    # Converts data from the string Url
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-            <h1> { date.day}-{date.month}-{date.year}</h1>
+            <h1>News for { date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
     return HttpResponse(html)
+
+def past_days_news(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+def convert_dates(dates):
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
