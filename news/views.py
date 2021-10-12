5e58a99e4958c66102227b4258230379672ae3fc
othienoJoe <bryo094@gmail.com>
2021-10-12 19:28:21 +0300

    styled and restructured the templates files

--- news/views.py
+++ news/views.py
@@ -6,7 +6,7 @@ import datetime as dt
 def welcome(request):
     return render(request, 'welcome.html')
 
-def news_of_day(request,past_date):
+def news_of_day(request):
     date = dt.date.today()
     return render(request, 'all-news/today-news.html', {"date": date,})
 
@@ -24,13 +24,3 @@ def past_days_news(request,past_date):
         return redirect(news_of_day)
 
     return render(request, 'all-news/past-news.html', {"date": date})
-
-def convert_dates(dates):
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
