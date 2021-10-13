3500ae71e89de170678defacec0523ccaaeabfbf
othienoJoe <bryo094@gmail.com>
2021-10-13 11:53:42 +0300

    made some updates in templates files,views and models

--- news/views.py
+++ news/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Article
+from .forms import NewsLetterForm, forms
 import datetime as dt
 
 # Create your views here.
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news, "letterForm": form})
 
 def past_days_news(request, past_date):
     try:
