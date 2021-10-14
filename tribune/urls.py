5b987e5e50e94edcdb1d17efea657dfa1738d82a
othienoJoe <bryo094@gmail.com>
2021-10-14 08:09:43 +0300

    created login file,updated project urls and also updated the navbar

--- tribune/urls.py
+++ tribune/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('news.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),    url(r'^logout/$', views.LogoutView.as_view(), {"next_page": '/'}),
 ]
